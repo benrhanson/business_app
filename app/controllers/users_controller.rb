40ac96470fa15c8920e123c8122ed6ffc3969198
Ben Hanson <benrhanson@live.com>
2016-06-19 06:03:37 +0000

    made sure users have to be logged in to edit a profile.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  
+  before_action :logged_in_user, only: [:edit, :update]  
+  before_action :correct_user,   only: [:edit, :update]  
   
   # displays the signed in user
   def show
@@ -45,5 +46,18 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :phone, :address, :zip_code, :city, :state, :career, :experience, :years_at_job, :bio)
   end
+  
+  # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end  
+  
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
+    end  
   
 end
